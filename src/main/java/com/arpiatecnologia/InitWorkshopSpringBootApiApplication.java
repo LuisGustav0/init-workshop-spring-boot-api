8e27978cc5e8dbbc3c7a8e8331084e9101b0f612
luisgustavo <luis.gustav0@live.com>
2021-04-10 13:59:16 -0300

    chore: iniciando projeto

---
+++ src/main/java/com/arpiatecnologia/InitWorkshopSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.arpiatecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitWorkshopSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitWorkshopSpringBootApiApplication.class, args);
+	}
+
+}
